df7e2ca4048da20b74916d1f199775bd44da2ccd
Piscki Pratama <pisckipratama@gmail.com>
2020-01-10 21:36:18 +0700

    Add Challange #7

---
+++ challange7/app-c7.js
@@ -0,0 +1,16 @@
+// challange 7 rubicamp
+
+function weirdMultiply(sentence) {
+    let changeToString = sentence.toString()
+    let data = changeToString.split('');
+
+    if (data.length === 1) {
+        return sentence;
+    }
+    let result = data.reduce((accumulator, currentValue) => accumulator * currentValue);
+    return weirdMultiply(result);
+}
+
+console.log(weirdMultiply(39)); // output 4
+console.log(weirdMultiply(999)); // output 2
+console.log(weirdMultiply(3)); // output 3
